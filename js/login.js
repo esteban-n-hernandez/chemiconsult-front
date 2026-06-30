327eb33a51058e9ec722ca302c98ecd2cd01cb6f
Hernandez, Esteban <ehern145@ford.com>
2026-06-30 14:19:00 -0300

    Refactor folders. Add login button google and microsoft

---
+++ js/login.js
@@ -0,0 +1,49 @@
+document.getElementById("loginForm").addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const formBody = new URLSearchParams();
+    formBody.append("email", email);
+    formBody.append("password", password);
+
+    try {
+        const response = await fetch("https://chemiconsult.onrender.com/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/x-www-form-urlencoded"},
+            body: formBody
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+
+            console.log("✅ Respuesta completa:", data);
+
+            localStorage.setItem("token", data.token);
+            localStorage.setItem("userEmail", email);
+            localStorage.setItem("userRole", data.role.toUpperCase());
+
+            const rol = data.role.toUpperCase();
+
+            if (rol === "ROLE_CLIENTE") {
+                window.location.href = "dashboard-cliente.html";
+            } else {
+                window.location.href = "dashboard-empleado.html";
+            }
+        } else {
+            document.getElementById("error-message").style.display = "block";
+        }
+    } catch (error) {
+        console.error("Error de conexión:", error);
+        document.getElementById("error-message").style.display = "block";
+    }
+});
+
+document.getElementById("msLoginBtn").addEventListener("click", function () {
+    window.location.href = "https://chemiconsult.onrender.com/oauth2/authorization/microsoft";
+});
+
+document.getElementById("googleLoginBtn").addEventListener("click", function () {
+    window.location.href = "https://chemiconsult.onrender.com/oauth2/authorization/google";
+});
